30a56cffd3cd42b300cd90533e47674b974c0f00
Hugooo007 <102212073+Hugooo007@users.noreply.github.com>
2022-03-23 09:06:05 +0100

    photorésistance
    préparation du code

--- main.ino
+++ main.ino
@@ -1,3 +1,18 @@
-int main() {
-    return 0
+int valeurCapteur=0;
+void setup()
+{
+    pinMode(A0,INPUT);
+    Serial.begin(9600);
+}
+void loop 
+{
+    valeurCapteur = analoogRead(A0);
+    Serial.pringtln(valeurCapteur);
+    if(valeurCapteur>=800)
+    {
+    }
+    else
+    {
+    }
+    delay(100);
 }
