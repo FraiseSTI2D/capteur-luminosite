dd714a73ba5411172e956f93b165b57d07511476
Hugooo007 <hugolefebvre95@icloud.com>
2022-04-01 08:53:28 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,11 +1,29 @@
 int valeurCapteur=0;
+long momentAVANT= 0;
+long momentPRESENT;
+int etatLUMI= LOW;
 void setup()
 {
     pinMode(A0,INPUT);
     Serial.begin(9600);
+    pinMode(13, OUTPUT);
 }
 void loop 
 {
+    momentAVANT= millis();
+    if (momentPRESENT-momentAVANT> 10000);
+    {
+        momentAVANT = momentPRESENT;
+        if (etatLUMI == LOW)
+        {
+            etatLUMI = HIGH;
+        }
+    }
+    else 
+    {
+        etatLUMI = LOW;
+    }
+    digitalWrite(13, etatLUMI);
     valeurCapteur = analoogRead(A0);
     Serial.pringtln(valeurCapteur);
     if(valeurCapteur>=800)
